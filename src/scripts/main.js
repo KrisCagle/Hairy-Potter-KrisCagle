1d3e660afd4017c476134a0c1af302b0bbaf4eaf
Kristofer Cagle <kriscagle@gmail.com>
2026-02-23 14:34:38 -0600

    Updated project and added new files for Kiln, PotteryCatalog, and PotteryList. Also updated main.js to create pottery pieces and log them to the console.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,18 @@
 // Imports go first
-
+import { makePottery } from "./PotteryWheel.js";
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
-
-
+let mug = makePottery("mug", 2, 4)
+console.log(mug);
+let bowl = makePottery("bowl", 3, 3)
+console.log(bowl);
+let plate = makePottery("plate", 1, 2)
+console.log(plate);
+let vase = makePottery("vase", 5, 6)
+console.log(vase);
+let candleHolder = makePottery("candle holder", 1.5, 3)
+console.log(candleHolder);
 // Fire each piece of pottery in the kiln
 
 
